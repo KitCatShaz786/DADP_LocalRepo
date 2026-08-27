cf8808e1dc8a2d8dfb1c7aca55dfadee66eb8c32
VescentKith <brat_style@hotmail.com>
2026-08-27 14:27:03 +0200

    Terrain
    - Added Terrain asset to Sample Scene

---
+++ DADPSem2ClassExamples/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PoolManager : MonoBehaviour
+{
+    public GameObject prefab;
+    public int poolSize = 10;
+
+    private List<GameObject> pool = new List<GameObject>();
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        for(int i = 0; i < pool.Count; i++)
+        {
+            if (!pool[i].activeInHierarchy)
+            {
+                pool[i].SetActive(true);
+                return pool[i];
+            }
+        }
+
+        for (int i = 0; i < pool.Count; i++)
+        {
+            pool[i].SetActive(false);
+        }
+
+        var obj = pool[0];
+        obj.SetActive(true);
+        return obj;
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
