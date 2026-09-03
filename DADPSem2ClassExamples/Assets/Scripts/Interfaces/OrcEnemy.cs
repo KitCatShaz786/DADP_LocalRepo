aaea1320765b017427c3005379bcd5715d45dc0a
VescentKith <brat_style@hotmail.com>
2026-09-03 14:38:52 +0200

    C# Interfaces
    - The scripts are all scuffed though

---
+++ DADPSem2ClassExamples/Assets/Scripts/Interfaces/OrcEnemy.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+/*
+public class OrcEnemy : MonoBehaviour, IDamageable
+{
+    public void TakeDamage(int amount)
+    {
+        Debug.Log("The Orc takes damage! It is now at " + health + " health.");
+    }
+}
+*/
---
+++ DADPSem2ClassExamples/Assets/Scripts/Interfaces/ZombieEnemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+/*
+public int health = 10;
+
+public class ZombieEnemy : MonoBehaviour, IDamageable
+{
+    public void TakeDamage(int amount)
+    {
+        Debug.Log("The Zombie takes damage! It is now at " + health + " health.");
+    }
+}
+*/
